1db4fbcc1ffc1771c96b2cb6209a980420456854
carolyne-wanjiru <shirogithenduka@gmail.com>
2024-06-16 10:50:02 +0300

    add the apis

--- src/profile/Profilepart.jsx
+++ src/profile/Profilepart.jsx
@@ -5,95 +5,230 @@ import { RiGitRepositoryFill } from "react-icons/ri";
 import { MdOutlineLocationOn } from "react-icons/md";
 import { LuGitFork } from "react-icons/lu";
 import { FaStar } from "react-icons/fa";
+import { IoLinkSharp } from "react-icons/io5";
 import UserDetails from "../store/store";
 import "./profile.css"
 
-const Profilepart = () =>{
+const Profilesection = ({profileimg, username, othername,Description,link,usersrepo,userfollower,userfollowing,}) =>{
       
     return (
-      <section className="profile-sector">
-        <div className="profile-section">
-          <div className="profile-logo">
-            <img src={profile}></img>
+      <>
+        <section className="profile-sector">
+          <div className="profile-section">
+            <div className="profile-logo">
+              <img src={profileimg}></img>
+            </div>
+            <div className="info-section">
+              <h2>{username}</h2>
+              <p>{othername}</p>
+              <p>{Description}</p>
+              <a href={link} target="_blank">
+                <button className="gitbtn">View On Github</button>
+              </a>
+              <p>
+                <MdOutlineLocationOn />
+                location
+              </p>
+              <p>
+                <RiGitRepositoryFill />
+                {usersrepo}repository
+              </p>
+              <p>
+                <MdGroups /> {userfollower} followers
+              </p>
+              <p>
+                <MdGroups />
+                {userfollowing} following
+              </p>
+            </div>
           </div>
-          <div className="info-section">
-            <h2>Github</h2>
-            <p>github</p>
-            <p>we create sites</p>
-            <button className="gitbtn">View On Github</button>
-            <p>
-              <MdOutlineLocationOn />
-              location
-            </p>
-            <p>
-              <RiGitRepositoryFill />
-              496 repos
-            </p>
-            <p>
-              <MdGroups /> 4860 followers
-            </p>
-            <p>
-              <MdGroups />0 following
-            </p>
+        </section>
+      </>
+    ); } 
+        
+const RepositoryPart = ({ title, description, forks, stars, repolink }) => {
+  return (
+    <>
+      <div className="repository-sector">
+        <h2 className="repo-header">Repository(30)</h2>
+        <div className="repo-section">
+          <a href={repolink} target="_blank"></a>
+          <div className="repository-card">
+            <div className="repo-name">
+              <p>{title}</p>
+              <p>{description}</p>
+            </div>
+            <div className="card-detail">
+              <p>
+                <LuGitFork />
+                {forks}forks
+              </p>
+              <p>
+                <FaStar />
+                {stars}stars
+              </p>
+            </div>
           </div>
         </div>
+      </div>
+    </>
+  );
+};
 
-        <div className="repository-sector">
-          <h2 className="repo-header">Repository(30)</h2>
-          <div className="repo-section">
-            <div className="repository-card">
-              <div className="repo-name">
-                <p>Name</p>
-                <p>Details</p>
+
+const FollowersPart = ({followersimage, followersname, details}) => {
+    return (
+      <>
+        <div className="followers-sector">
+          <h2 className="followers-header">Repository(30)</h2>
+          <div className="followers-section">
+            <div className="followers-card">
+              <div className="followers-logo">
+                <img src={followersimage}></img>
               </div>
               <div className="card-detail">
-                <p>
-                  <LuGitFork />
-                
-                  2360 forks
-                </p>
-                <p>
-                  <FaStar />
-                
-                  819 stars
-                </p>
+                <p>{followersname}</p>
+                <button onClick={() => details(followersname)}>
+                  {<IoLinkSharp />}
+                  view {followersname}
+                </button>
               </div>
             </div>
-            {/* <div className="repository-card">
-              <div className="repo-name">
-                <p>Name</p>
-                <p>Details</p>
-              </div>
-              <div className="card-detail">
-                <p>
-                  <LuGitFork />
-                  2360 forks
-                </p>
-                <p>
-                  <FaStar />
-                  819 stars
-                </p>
-              </div>
-            </div> */}
-            {/* <div className="repository-card">
-              <div className="repo-name">
-                <p>Name</p>
-                <p>Details</p>
-              </div>
-              <div className="card-detail">
-                <p>
-                  <LuGitFork />
-                  2360 forks
-                </p>
-                <p>
-                  <FaStar />
-                  819 stars
-                </p>
-              </div>
-            </div> */}
           </div>
         </div>
-      </section>
+      </>
     );
-  }
+  };
+
+ 
+const Profilepart =()=>{
+   const username = UserDetails((state) => state.username);
+   const userinfo = UserDetails((state) => state.userinfo);
+   const defaultValue = UserDetails((state) => state.fetchuserdata);
+   const [usersrepo, setUserrepos] = useState([]);
+   const [userfollower, setUserfollowers] = useState([]);
+   const [userfollowing, setUserfollowing] = useState([]);
+   const [isLoading, setLoading] = useState(true);
+
+      useEffect(() => {
+        defaultValue(username);
+      }, [username, defaultValue]);
+
+       const getdetails = async () => {
+         setLoading(true);
+         if (username) {
+           try {
+             const usersrepo = await fetch(
+               `https://api.github.com/users/${username}/repos`
+             );
+             if (usersrepo == null) {
+               console.log("no repository found");
+             }
+             const userreposresult = await usersrepo.json();
+             setUserrepos(userreposresult);
+
+             const userfollower = await fetch(
+               `https://api.github.com/users/${username}/followers`
+             );
+              if (userfollower == null) {
+                console.log("no followers found");
+              }
+             const userfollowerResult = await userfollower.json();
+             setUserfollowers(userfollowerResult);
+
+             const userfollowing = await fetch(
+               `https://api.github.com/users/${username}/following`
+             );
+             const userfollowingResult = await userfollowing.json();
+             setUserfollowing(userfollowingResult);
+
+           } catch (error) {
+             console.log("Failed to fetch user followings", error);
+           }
+         } else {
+           return <div>No available data to display</div>;
+         }
+         setLoading(false);
+       };
+
+       const details = (username) => {
+         defaultValue(username);
+         getdetails(username);
+       };
+
+       useEffect(() => {
+        getdetails();
+       }, [username]);
+
+    return (
+      <>
+        <div className="profile-sector">
+          {isLoading ? (
+            <p className="loading">Loading profile....</p>
+          ) : (
+            userinfo && (
+              <Profilesection
+                profileimg={userinfo.avatar_url}
+                username={userinfo.name}
+                othername={userinfo.login}
+                Description={userinfo.bio}
+                link={userinfo.html_url}
+                usersrepo={userinfo.public_repos}
+                userfollower={userinfo.followers}
+                userfollowing={userinfo.following}
+              />
+            )
+          )}
+
+          <div className="repository-sector">
+            {isLoading ? (
+              <p className="loading">Loading GitHub's repositories......</p>
+            ) : (
+              <div className="repo-section">
+                {usersrepo.map((repos, i) => (
+                  <RepositoryPart
+                    key={i}
+                    title={repos.name}
+                    description={repos.description}
+                    forks={repos.forks}
+                    stars={repos.stars}
+                    repolink={repos.clone_url}
+                  />
+                ))}
+              </div>
+            )}
+            {isLoading ? (
+              <p className="loading">Fetching followers......</p>
+            ) : (
+              <div className="FollowersPart">
+                {userfollower.map((followers, i) => (
+                  <FollowersPart
+                    key={i}
+                    followersimage={followers.avatar_url}
+                    followersname={followers.login}
+                    details={details}
+                  />
+                ))}
+              </div>
+            )}
+
+            {isLoading ? (
+              <p className="loading">Fetching following.....</p>
+            ) : (
+              <div className="followersSect">
+                {userfollowing.map((following, i) => (
+                  <FollowersPart
+                    followerImg={following.avatar_url}
+                    followerName={following.login}
+                    details={details}
+                  />
+                ))}
+              </div>
+            )}
+          </div>
+        </div>
+      </>
+    );
+};
+       
 export default Profilepart;
--- src/store/store.js
+++ src/store/store.js
@@ -1,37 +1,57 @@
 import { create } from "zustand";
 
 const UserDetails = create((set)=>({
-    Profile:[],
-    repository:[],
-    followers:[],
-    following:[],
 
-    fetchUserProfile: async()=>{
-        const fetchUserProfile = await fetch(`https://api.github.com/users/githenduka-carolyne`);
-        const UserProfile = await fetchUserProfile.json();
+username: "github",
+userinfo: {},
 
-        set({UserProfile})
-    },
-    fetchUserRepo: async()=>{
-        const fetchUserRepo = await fetch(`https://api.github.com/users/githenduka-carolyne/repos`);
-        const UserRepo = await fetchUserRepo.json();
+ setusername: (username) => set({ username }),
+ setuserinfo: (userinfo) => set({ userinfo }),
 
-        set({UserRepo})
-    },
-    fetchUserFollowers: async()=>{
-        const fetchUserFollowers = await fetch(`https://api.github.com/users/Githenduka-carolyne/followers`);
-        const UserFollowers = await fetchUserFollowers.json();
-
-        set({UserFollowers})
-    },
-    fetchUserFollowing: async()=>{
-        const fetchUserFollowing = await fetch(`https://api.github.com/users/Githenduka-carolyne/following{/other_user}`);
-        const UserFollowing = await fetchUserFollowing.json();
-
-        set({UserFollowing})
+   fetchuserdata: async (username) => {
+    if (!username) {
+      return;
     }
+    try {
+      const results = await fetch(
+        `https://api.github.com/users/${username}`
+      );
+      const data = await results.json();
+
+      set({ userdata: data, username });
+    } catch (error) {
+      console.error("Failed to fetch user's information", error);
+    }
+  },
+    
+
+ }));
+//     fetchUserProfile: async()=>{
+//         const fetchUserProfile = await fetch(`https://api.github.com/users/${username}`);
+//         const UserProfile = await fetchUserProfile.json();
+
+//         set({UserProfile})
+//     },
+//     fetchUserRepo: async()=>{
+//         const fetchUserRepo = await fetch(`https://api.github.com/users/${username}/repos`);
+//         const UserRepo = await fetchUserRepo.json();
+
+//         set({UserRepo})
+//     },
+//     fetchUserFollowers: async()=>{
+//         const fetchUserFollowers = await fetch(`https://api.github.com/users/${username}/followers`);
+//         const UserFollowers = await fetchUserFollowers.json();
+
+//         set({UserFollowers})
+//     },
+//     fetchUserFollowing: async()=>{
+//         const fetchUserFollowing = await fetch(`https://api.github.com/users/${username}/following`);
+//         const UserFollowing = await fetchUserFollowing.json();
+
+//         set({UserFollowing})
+//     }
 
 
 
-}));
+// }));
 export default UserDetails;
